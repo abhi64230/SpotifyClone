8c493c35ca065a59b4f2ad2c56f7eee52e6f735c
Abhishek Pradhan <138620027+abhi64230@users.noreply.github.com>
2024-12-11 21:49:26 +0530

    Update index.js

--- spotify_backend/index.js
+++ spotify_backend/index.js
@@ -1,60 +1,78 @@
- const express = require("express");
- const mongoose = require("mongoose");
- const JwtStrategy = require('passport-jwt').Strategy;
- const ExtractJwt = require('passport-jwt').ExtractJwt;
- const passport = require('passport');
- const User = require('./models/User');
- const authRoutes = require("./routes/auth");
- const songRoutes = require("./routes/song");
- const playlistRoutes = require("./routes/playlist");
- require("dotenv").config();
- const cors = require("cors");
- const app = express();
- const port = 8000;
- app.use(cors());
- app.use(express.json());
+const express = require("express");
+const mongoose = require("mongoose");
+const JwtStrategy = require("passport-jwt").Strategy;
+const ExtractJwt = require("passport-jwt").ExtractJwt;
+const passport = require("passport");
+const User = require("./models/User");
+const authRoutes = require("./routes/auth");
+const songRoutes = require("./routes/song");
+const playlistRoutes = require("./routes/playlist");
+require("dotenv").config();
+const cors = require("cors");
 
-mongoose.connect("mongodb+srv://Abhishek_2001:" +
-                process.env.MONGO_PASSWORD +
-    "@cluster0.ss5qu.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
-    {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-    }
+const app = () => {
+  try {
+    const app = express();
+    const port = 8000;
+    app.use(cors());
+    app.use(express.json());
+
+    mongoose.connect("mongodb+srv://Abhishek_2001:" +
+      process.env.MONGO_PASSWORD +
+"@cluster0.ss5qu.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
+{
+useNewUrlParser: true,
+useUnifiedTopology: true,
+}
 )
 .then((x) =>{
 console.log ("connected to Mongo!");
 })
 .catch((err) =>{
-    console.log("Error while connecting to Mongo");
+console.log("Error while connecting to Mongo");
 });
 
-// setup passport-jwt
 
-let opts = {};
-opts.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
-opts.secretOrKey = "thisKeyIsSupposedToBeSecret";
-passport.use(
-    new JwtStrategy(opts,async (jwt_payload,done)=>{
-        try{
-            const user=await User.findOne({id:jwt_payload.sub})
-            if(user) return done(null,user);
-            else return done(null,false);
-        }catch(err){
-            return done(err,false);
+    // mongoose
+    //   .connect(process.env.MONGO_URI)
+    //   .then((x) => {
+    //     console.log("connected to Mongo!");
+    //   })
+    //   .catch((err) => {
+    //     console.log("Error while connecting to Mongo");
+    //   });
+
+    // setup passport-jwt
+
+    let opts = {};
+    opts.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
+    opts.secretOrKey = "thisKeyIsSupposedToBeSecret";
+    passport.use(
+      new JwtStrategy(opts, async (jwt_payload, done) => {
+        try {
+          const user = await User.findOne({ id: jwt_payload.sub });
+          if (user) return done(null, user);
+          else return done(null, false);
+        } catch (err) {
+          return done(err, false);
         }
-    })
-)
+      })
+    );
 
-app.get("/",(req,res)=>{
-    res.send("Hello World");
-});
-app.use("/auth", authRoutes);
-app.use("/song", songRoutes);
-app.use("/playlist", playlistRoutes);
+    app.get("/", (req, res) => {
+      res.send("Hello World");
+    });
+    app.use("/auth", authRoutes);
+    app.use("/song", songRoutes);
+    app.use("/playlist", playlistRoutes);
 
-// Now we want to tell express that our sever will run on localhost : 8000
-app.listen(port,()=>{
-    console.log("App is running on port  " + port);  
-});
+    // Now we want to tell express that our sever will run on localhost : 8000
+    app.listen(port, () => {
+      console.log("App is running on port  http://localhost:" + port);
+    });
+  } catch (error) {
+    console.log(error)
+  }
+};
 
+app();

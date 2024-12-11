4645d8dc9d3221bb3fe8dd38d45a30b231adbb02
Abhishek Pradhan <138620027+abhi64230@users.noreply.github.com>
2024-12-11 21:53:26 +0530

    Update playlist.js

--- spotify_backend/routes/playlist.js
+++ spotify_backend/routes/playlist.js
@@ -1,4 +1,4 @@
-const express =require("express");
+const express = require("express");
 const passport = require("passport");
 const Playlist = require("../models/Playlist");
 const User = require("../models/User");
@@ -6,75 +6,128 @@ const Song = require("../models/Song");
 
 const router = express.Router();
 
-router.post("/create", passport.authenticate("jwt",{session:false}),
-async (req,res) => {
-    const currentUser = req.user;
-     const {name, thumbnail,song} = req.body;
-     if(!name || !thumbnail || !song) {
-        return res.status(301).json({err: "Insufficient data"});
-     }
-     const playlistData = {name, thumbnail,song, owner:currentUser._id, collaborators:[],};
+router.post(
+  "/create",
+  passport.authenticate("jwt", { session: false }),
+  async (req, res) => {
+    try {
+      const currentUser = req.user;
+      const { name, thumbnail,songs } = req.body;
+      if (!name || !thumbnail ||!songs) {
+        return res.status(301).json({ err: "Insufficient data" });
+      }
+      const playlistData = {
+        name,
+        thumbnail,
+        songs,
+        owner: currentUser._id,
+        collaborators: [],
+      };
 
-     const playlist = await Playlist.create(playlistData);
-     return res.status(200).json(playlist);
-}
+      const playlist = await Playlist.create(playlistData);
+      return res.status(200).json(playlist);
+    } catch (error) {
+      console.log(JSON.stringify(error));
+    }
+  }
 );
 
-router.get("/get/playlist/:playlistId", passport.authenticate("jwt",{session:false}),
-async(req,res)=>{
-    const playlistId = req.params.playlistId;
-    const playlist = await Playlist.findOne({_id: playlistId});
-    if(!playlist) {
-        return res.status(301).json({err: "Invalid ID"});
+router.get(
+  "/get/playlist/:playlistId",
+  passport.authenticate("jwt", { session: false }),
+  async (req, res) => {
+    try {
+      const playlistId = req.params.playlistId;
+      const playlist = await Playlist.findOne({ _id: playlistId });
+      if (!playlist) {
+        return res.status(301).json({ err: "Invalid ID" });
+      }
+      return res.status(200).json(playlist);
+    } catch (error) {
+      console.log(error);
     }
-    return res.status(200).json(playlist);
-}
-); 
-
-router.get("/get/me", passport.authenticate("jwt",{session:false}),
-async(req,res) => {
-
-    const artistId =req.user._id;
-
-    // const artist = await User.findOne({_id:artistId});
-    if(!artist){
-        return res.status(304).json({err: "Invalid Artist ID"});
-    }
-
-    const playlists = await Playlist.find({owner:artistId}).populate('owner');
-    return res.status(200).json({data: playlists});
-}
+  }
 );
 
-router.post("/add/song", passport.authenticate("jwt",{session:false}),
-async(req,res) => {
-const currentUser =req.user;
-const {playlistId, songId} = req.body;
+router.get(
+  "/get/me",
+  passport.authenticate("jwt", { session: false }),
+  async (req, res) => {
+    try {
+      const artistId = req.user._id;
 
-const playlist = await Playlist.findOne({_id:playlistId});
-if(!playlist) {
-    return res.status(304).json({err: "Playlist dose not exist"});
+      // const artist = await User.findOne({_id:artistId});
+      // if (!artist) {
+      //   return res.status(304).json({ err: "Invalid Artist ID" });
+      // }
+
+      const playlists = await Playlist.find({ owner: artistId }).populate(
+        "owner"
+      );
+      return res.status(200).json({ data: playlists });
+    } catch (error) {
+      console.log(error);
     }
+  }
+);
 
-    if(
+router.get(
+  "/get/artist/:artistId",
+  passport.authenticate("jwt", {session: false}),
+  async (req, res) => {
+    try{
+      const artistId = req.params.artistId;
+
+      // We can do this: Check if artist with given artist Id exists
+      const artist = await User.findOne({_id: artistId});
+      if (!artist) {
+          return res.status(304).json({err: "Invalid Artist ID"});
+      }
+
+      const playlists = await Playlist.find({owner: artistId});
+      return res.status(200).json({data: playlists});
+    }catch(error){
+      console.log(error);
+    }
+    
+  }
+);
+
+
+
+router.post(
+  "/add/song",
+  passport.authenticate("jwt", { session: false }),
+  async (req, res) => {
+    try {
+      const currentUser = req.user;
+      const { playlistId, songId } = req.body;
+
+      const playlist = await Playlist.findOne({ _id: playlistId });
+      if (!playlist) {
+        return res.status(304).json({ err: "Playlist dose not exist" });
+      }
+
+      if (
         !playlist.owner.equals(currentUser._id) &&
         !playlist.collaborators.includes(currentUser._id)
-    )
-    {
-    return res.status(400).json({err:"Not allowed"})
+      ) {
+        return res.status(400).json({ err: "Not allowed" });
+      }
+
+      const song = await Song.findOne({ _id: songId });
+      if (!song) {
+        return res.status(304).json({ err: "Song does not exist" });
+      }
+
+      playlist.songs.push(songId);
+      await playlist.save();
+
+      return res.status(200).json(playlist);
+    } catch (error) {
+      console.log(error);
+    }
   }
-
-  const song = await Song.findOne({_id:songId});
-  if(!song) {
-    return res.status(304).json({err: "Song does not exist"});
-  }
-
-  playlist.songs.push(songId);
-  await playlist.save();
-
-  return res.status(200).json(playlist);
-
-}
 );
 
 module.exports = router;

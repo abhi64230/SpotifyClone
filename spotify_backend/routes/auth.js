ae5580448a34dea4516a081dfa198c50b6b44dfe
Abhishek Pradhan <138620027+abhi64230@users.noreply.github.com>
2024-12-11 21:54:31 +0530

    Update auth.js

--- spotify_backend/routes/auth.js
+++ spotify_backend/routes/auth.js
@@ -2,49 +2,58 @@ const express = require("express");
 const router = express.Router();
 const User = require("../models/User");
 const bcrypt = require("bcrypt");
-const {getToken} = require("../utils/helpers");
+const { getToken } = require("../utils/helpers");
 
+router.post("/register", async (req, res) => {
+  try {
+    const { email, password, firstName, lastName, username } = req.body;
 
-router.post("/register",async (req,res)=>{
- const {email,password,firstName,lastName,username}=req.body;
+    const user = await User.findOne({ email: email });
+    if (user) {
+      return res
+        .status(403)
+        .json({ error: "A user with this email already exists" });
+    }
 
- const user = await User.findOne({email:email});
- if(user){
-    return res.status(403).json({error:"A user with this email already exists"});
- }
+    const hashedPassword = bcrypt.hash(password, 10);
+    const newUserData = {
+      email,
+      password: String(hashedPassword),
+      // password: hashedPassword,
+      firstName,
+      lastName,
+      username,
+    };
+    const newUser = await User.create(newUserData);
 
- const hashedPassword = bcrypt.hash(password,10); 
- const newUserData = {
-   email,
-    password:String(hashedPassword),
-   // password: hashedPassword,
-   firstName,
-   lastName,
-   username,};
- const newUser = await User.create(newUserData);
+    const token = await getToken(email, newUser);
 
- const token = await getToken(email,newUser);
-
- const userToReturn = {...newUser.toJSON(), token};
- delete userToReturn.password;
- return res.status(200).json(userToReturn);
+    const userToReturn = { ...newUser.toJSON(), token };
+    delete userToReturn.password;
+    return res.status(200).json(userToReturn);
+  } catch (error) {
+    console.log(error);
+  }
 });
-router.post("/login", async (req,res) => {
-   const {email,password}=req.body;
-   const user = await User.findOne({email:email});
-   if(!user){
-      return res.status(403).json({err:"Invalid credentials"}); 
-   }
+router.post("/login", async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email: email });
+    if (!user) {
+      return res.status(403).json({ err: "Invalid credentials" });
+    }
 
-   const isPasswordValid = await bcrypt.compare(password,user.password);
-   if(!isPasswordValid){
-      return res.status(403).json({err:"Invalid credentials"});
-      }
-      const token = await getToken(user.email, user);
-      const userToReturn = {...user .toJSON(), token};
-      delete userToReturn.password;
-      return res.status(200).json(userToReturn);
-
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+    if (!isPasswordValid) {
+      return res.status(403).json({ err: "Invalid credentials" });
+    }
+    const token = await getToken(user.email, user);
+    const userToReturn = { ...user.toJSON(), token };
+    delete userToReturn.password;
+    return res.status(200).json(userToReturn);
+  } catch (error) {
+    console.log(error);
+  }
 });
 
-module.exports=router;
+module.exports = router;
